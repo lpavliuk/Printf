010c6d456cee8481b7d83f968ac7818d74b5f28e
Aleksey Pavliuk <pavliuk.aleksey@gmail.com>
2018-04-20 15:56:29 +0300

    changed

--- ft_printf.c
+++ ft_printf.c
@@ -6,97 +6,35 @@
 /*   By: opavliuk <opavliuk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/17 15:37:14 by opavliuk          #+#    #+#             */
-/*   Updated: 2018/04/20 11:26:36 by opavliuk         ###   ########.fr       */
+/*   Updated: 2018/04/20 15:55:36 by opavliuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include <stdio.h>
 
-// int		separator(char c)
-// {
-// 	if (c != 's' && c != 'S' && c != 'p' && c != 'd'
-// 		&& c != 'D' && c != 'i' && c != 'o' &&
-// 		c != 'O' && c != 'u' && c != 'U' &&
-// 		c != 'x' && c != 'X' && c != 'c' && c != 'C')
-// 		return (1);
-// 	else
-// 		return (0);
-// }
+void	write_type_c(const char *format, t_str pf)
+{
+	
+}
 
-// int		check_modifier(const char *format, int i, t_str *pf)
-// {
-// 	int		k;
-
-// 	k = 0;
-// 	pf->modifier[k] = format[i];
-// 	k++;
-// 	i++;
-// 	if ((format[i] == 'l' && format[i - 1] == 'l')
-// 		|| (format[i] == 'h' && format[i - 1] == 'h'))
-// 	{
-// 		pf->modifier[k] = format[i];
-// 		k++;
-// 		i++;
-// 	}
-// 	if (!separator(format[i]))
-// 		return (i);
-// 	else
-// 	{
-// 		ft_strclr(pf->modifier);
-// 		return (0);
-// 	}
-// }
-
-// void	check_flags(const char *format, int *i, t_str *pf)
-// {
-// 	int k;
-
-// 	k = 0;
-// 	if (format[*i] == '+' || format[*i] == '-')
-// 	{
-// 		pf->flags[k] = format[*i];
-// 		k++;
-// 		(*i)++;
-// 	}
-// 	if (format[*i] == '#')
-// 	{
-// 		pf->flags[k] = format[*i];
-// 		(*i)++;
-// 		k++;
-// 	}
-// 	if (format[*i] == '0' && format[*i - k] != '-')
-// 		pf->flags[k] = format[*i];
-// }
-
-// int		check_percent(const char *format, t_str *pf)
-// {
-// 	int	i;
-
-// 	i = 1;
-// 	check_flags(format, &i, pf);
-// 	pf->width = ft_atoi(format + i);
-// 	while (separator(format[i]) && format)
-// 	{
-// 		if (format[i] == '.')
-// 			pf->precision = ft_atoi(format + (++i));
-// 		else if (format[i] == 'h' || format[i] == 'l'
-// 		 	|| format[i] == 'j' || format[i] == 'z')
-// 		{
-// 			if ((i = check_modifier(format, i, pf)) != 0)
-// 				break ;
-// 			else
-// 				return (0);
-// 		}
-// 		else if (!ft_isdigit(format[i]) && format[1] != '+'
-// 			&& format[1] != '-')
-// 			return (0);
-// 		i++;
-// 	}
-// 	if (!separator(format[i]))
-// 		pf->type = format[i];
-// 	return (i);
-// }
+void	check_type(const char *format, t_str pf)
+{
+	if (pf->type == 'c' || pf->type == 'C')
+		write_type_c(format, pf);
+	else if (pf->type == 'd' || pf->type == 'D')
+		write_type_d(format, pf);
+	else if (pf->type == 'i')
+		write_type_i(format, pf);
+	else if (pf->type == 'o' || pf->type == 'O')
+		write_type_o(format, pf);
+	else if (pf->type == 'x' || pf->type == 'X')
+		write_type_x(format, pf);
+	else if (pf->type == 'u' || pf->type == 'U')
+		write_type_u(format, pf);
+	else if (pf->type == 'p')
+		write_type_p(format, pf);
+}
 
 int		ft_printf(const char *format, ...)
 {
@@ -119,33 +57,14 @@ int		ft_printf(const char *format, ...)
 				format++;
 			else
 				format += i;
+			check_type(format, pf);
 		}
 		else if (*format == '%' && *(format + 1) == '%')
 			format++;
 		ft_putchar(*format);
 		format++;
 	}
-	ft_putchar('\n');
-	// while (format && *format != '\0')
-	// {
-	// 	if (*format == '%' && *(format + 1) != '%')
-	// 	{
-	// 		i = check_percent(format, pf);
-	// 		if (i == 0)
-	// 		{
-	// 			format++;
-	// 			continue ;
-	// 		}
-	// 		format += i + 1;
-	// 	}
-	// 	if (*format == '%' && *(format + 1) == '%')
-	// 		format++;
-	// 	ft_putchar(*format);
-	// 	// else
-	// 	// 	ft_putchar(*format);
-	// 	format++;
-	// }
-	// ft_putchar('\n');
+	va_end (ap);
 	if (i != 0)
 	{
 		printf("flags: %s\n", pf->flags);
@@ -157,6 +76,5 @@ int		ft_printf(const char *format, ...)
 	}
 	else
 		printf("Not valid percent or absent!\n");
-	va_end (ap);
 	return (0);
 }
